234fe2ae0e1847575e224d846a26326373fea59a
Pavel Oreshin <nutzpro@gmail.com>
2015-03-28 21:26:18 +0300

    add stanings need to fix delete 2 when come to standings

--- routes.js
+++ routes.js
@@ -125,20 +125,85 @@ module.exports = function(app) {
 
 
 
-    app.get('/score/', function(req, res) {
+    app.get('/score', function(req, res) {
 
         var _get = url.parse(req.url, true).query;
         //get winner photo name
         var winner = _get['winner'];
+        //get loser photo name
+        var loser = _get['loser'];
+        console.log(_get);
+        console.log(loser);
 
+        var winner_rating = 0;
+        var loser_rating = 0;
+        var winnerAdjustment = 0;
+        var loserAdjustment = 0;
+        var k = 20;
 
         //find winner ratings
         photos.findOne({name: winner}, function(err, found1) {
-            users.update({ip: req.ip}, {$addToSet: {battle: found1}}, function()
-            {
-                res.redirect('/tournament');
+
+
+
+            winner_rating = found1.ratings;
+
+
+
+            //find loser ratings
+            photos.findOne({name: loser}, function(err, found2) {
+
+
+
+                loser_rating = found2.ratings;
+
+                //Elo's rating algorithm
+                winnerExpected = 1 / (1 + (Math.pow(10, (loser_rating - winner_rating) / 400)));
+                loserExpected = 1 / (1 + (Math.pow(10, (winner_rating - loser_rating) / 400)));
+
+
+
+                winnerAdjustment = Math.round(winner_rating + (k * (1 - winnerExpected)));
+                loserAdjustment = Math.round(loser_rating + (k * (0 - loserExpected)));
+
+
+
+
+                photos.find({name: winner}, function(err, found_win) {
+
+                    if (found_win.length == 1) {
+                        //update new winner ratings
+                        photos.update(found_win[0], {$set: {ratings: winnerAdjustment}}, function(err) {
+                            //mark photo voted by user
+                            users.update({ip: req.ip}, {$addToSet: {battle: found1}}, function(err) {
+                                photos.find({name: loser}, function(err, found_los) {
+                                    if (found_los.length == 1) {
+                                        //update new loser ratings
+                                        photos.update(found_los[0], {$set: {ratings: loserAdjustment}}, function(err) {
+                                            res.redirect('/tournament');
+                                        });
+                                    }
+                                });
+                            });
+                        });
+                    }
+                });
+
             });
         });
+    });
+
+
+    app.get('/standings', function(req, res) {
+
+        photos.find({type: "kitten"}).sort({ratings: -1}).exec(function(err, all_photos) {
+
+
+            // render the standings page and pass the photos
+            res.render('standings', {standings: all_photos});
+
+        });
+
     });
 };
 
